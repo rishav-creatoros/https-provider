2aba77b9c565dd2440529cd3e65cea7bbd622734
Rishav Goenka <rishavgoenka8@gmail.com>
2023-06-05 10:01:06 +0530

    removed node modules

--- node_modules/@reclaimprotocol/reclaim-sdk/src/ReclaimProtocol/index.ts
+++
@@ -1,1 +0,0 @@
-export { Reclaim } from './Reclaim'
--- node_modules/@reclaimprotocol/reclaim-sdk/src/index.ts
+++
@@ -1,5 +0,0 @@
-import * as reclaimprotocol from './reclaim'
-
-export * from './types'
-export { reclaimprotocol }
-export { utils } from './reclaim'
